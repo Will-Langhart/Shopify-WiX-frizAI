d846dc3caebfbf3ee23d3a28ed4898dfa0fc8385
William Langhart <theholotrout@gmail.com>
2023-11-03 23:33:23 -0400

    Create .bash

---
+++ .bash
@@ -0,0 +1,13 @@
+         |       Wix       |     Shopify
+---------|-----------------|----------------------
+Basic    | $14/month       | $29/month
+         | (Website Plan)  | (Basic Shopify)
+---------|-----------------|----------------------
+Standard | $23/month       | $79/month
+         | (Business Basic)| (Shopify)
+---------|-----------------|----------------------
+Advanced | $39/month       | $299/month
+         | (Business VIP)  | (Advanced Shopify)
+---------|-----------------|----------------------
+Pro      | $49/month       | $2000+/month (Plus)
+         | (eCommerce Plan)| (Shopify Plus)
