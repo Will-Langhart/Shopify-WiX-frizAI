c971552b4c177d17b17dff56121ab2dad34055c5
William Langhart <theholotrout@gmail.com>
2023-11-03 23:35:03 -0400

    Update .bash

--- .bash
+++ .bash
@@ -1,13 +1,34 @@
-         |       Wix       |     Shopify
----------|-----------------|----------------------
-Basic    | $14/month       | $29/month
-         | (Website Plan)  | (Basic Shopify)
----------|-----------------|----------------------
-Standard | $23/month       | $79/month
-         | (Business Basic)| (Shopify)
----------|-----------------|----------------------
-Advanced | $39/month       | $299/month
-         | (Business VIP)  | (Advanced Shopify)
----------|-----------------|----------------------
-Pro      | $49/month       | $2000+/month (Plus)
-         | (eCommerce Plan)| (Shopify Plus)
+         |       Wix Plans          |     Shopify Plans
+------------------------------------------------------------------------------------
+Tier     | Plan Name & Cost/month   | Plan Name & Cost/month
+------------------------------------------------------------------------------------
+Entry    | Combo: $14               | Basic Shopify: $29
+         | - 2 GB Bandwidth         | - Unlimited products
+         | - 3 GB Storage           | - 2 Staff accounts
+         | - 30 Video Minutes       | - Up to 4 inventory locations
+------------------------------------------------------------------------------------
+Basic    | Unlimited: $18           | Shopify: $79
+Business | - Unlimited Bandwidth    | - Unlimited products
+         | - 10 GB Storage          | - 5 Staff accounts
+         | - 1 Hour Video           | - Up to 5 inventory locations
+         | - $300 Ad Vouchers       | - Professional reports
+------------------------------------------------------------------------------------
+Standard | Business Basic: $23      | Advanced Shopify: $299
+Business | - 20 GB Storage          | - Unlimited products
+         | - 5 Hours Video          | - 15 Staff accounts
+         | - Accept online payments | - Up to 8 inventory locations
+         | - Unlimited Products     | - Advanced report builder
+------------------------------------------------------------------------------------
+Advanced | Business Unlimited: $27  | Shopify Plus: Starts at $2000
+Business | - 35 GB Storage          | - Enterprise-grade solutions for high volume merchants
+         | - 10 Hours Video         | - Unlimited staff accounts
+         | - Subscriptions & Pricing | - Advanced apps and features
+         | - Multiple currencies     | - Dedicated support
+------------------------------------------------------------------------------------
+Pro      | Business VIP: $49        | 
+E-Commerce| - 50 GB Storage         | 
+         | - Unlimited Video        | 
+         | - Priority Support       | 
+         | - Custom Reports         | 
+------------------------------------------------------------------------------------
+Note: "Unlimited" in Wix plans refers to the number of products you can sell, not to be confused with bandwidth or storage.
